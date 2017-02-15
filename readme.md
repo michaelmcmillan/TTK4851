161de81746cf2bddeff62bad422881aa98e9a9aa
Michael McMillan <email@michaelmcmillan.net>
2017-02-15 16:17:18 +0100

    added initial install script

--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
 # TTK4851
 
+## Installering
+For å komme i gang med utvikling må du ha et par avhengigheter installert. Du kan kjøre følgende
+kommando fra terminalen på maskinen din for å installere det som kreves.
+
+````
+python ./scripts/install.py
+````
+
 ## Object recognition
 
 ## Control system
---
+++ script/install.py
@@ -0,0 +1,15 @@
+from sys import version_info
+
+# python version check
+if not version_info.major == 2 and not version_info.minor == 7:
+    raise Exception('Prosjektet krever at du har Python 2.7 installert.')
+
+# pip version check
+
+# check if virtualenv is installed
+
+# check if env exists
+
+# create env if does not exist
+
+# activate env and install lego-nxt + write to requirements
