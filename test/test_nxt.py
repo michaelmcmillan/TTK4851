924d8ae19086b36dbc32a53773d55665b4020e08
Michael McMillan <email@michaelmcmillan.net>
2017-02-22 12:05:33 +0100

    rename the walker test

--- test/test_nxt.py
+++ test/test_nxt.py
@@ -3,6 +3,6 @@ from control_system.brick_class import Walker
 
 class TestNXT(TestCase):
 
-    def test_nxt_can_use_the_bluetooth_driver(self):
+    def test_the_walker_can_move_forward(self):
         walker = Walker()
         walker.move()
