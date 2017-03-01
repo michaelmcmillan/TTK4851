e22fbb85391c4a3a5f8214346c0679e43c19932e
Admin <haukmj@icloud.com>
2017-03-01 14:32:01 +0100

    Fikset småfeil

--- src/control_system/controlloop.py
+++ src/control_system/controlloop.py
@@ -1,48 +1,53 @@
 import math
 import controller
 import pidvariables
+import brick_class
 import nxt_functions
-
-
-# Create PID variables
-PIDvar = pidvariables()
-
-# Create Controllers
-posistion_controller = controller.PID(2.5, .1, 1.5)
-posistion_controller.setSampleTime(0.1)
-posistion_controller.setSetpoint(0)
-
-angle_controller = controller.PID(2.5, .1, 0)
-angle_controller.setSampleTime(0.1)
 
 # Create flags for desicions
 waypoint_flag = False
 off_cource_flag = False
 THREASHOLD_POS = 2
 THREASHOLD_ANG = 2
+SAMPLE_TIME = 0.1
+
+# Create PID variables
+PIDvar = pidvariables.PidVariables()
+
+# Create Controllers
+posistion_controller = controller.PID(2.5, .1, 1.5)
+posistion_controller.setSampleTime(SAMPLE_TIME)
+posistion_controller.setSetpoint(0)
+
+angle_controller = controller.PID(2.5, .1, 0)
+angle_controller.setSampleTime(SAMPLE_TIME)
+
+walker=brick_class.Walker()
 
 
-
-while():
+while True:
     # Update coordinates
     PIDvar.update_coordinates()
 
     # Get robot/ref distance and angle
-    dist_rob = PIDvar.get_dist_robot
-    ang_rob = PIDvar.get_ang_robot                                      # From Compass
-    ang_ref = PIDvar.get_ang_ref                                        # Calculated from coordinates
+    dist_rob = PIDvar.get_dist_robot()
+    ang_rob = PIDvar.get_ang_robot()                                      # From Compass
+    ang_ref = PIDvar.get_ang_ref()                                        # Calculated from coordinates
+    print(ang_rob)
     ang_offset = math.fabs(ang_rob)-math.fabs(ang_ref)                  # Offset in angle
 
     # Angle controller
     if ang_offset > THREASHOLD_ANG:
         posistion_controller.clear()                                    # Reset posistion controller
-        ang_ctrl_output = angle_controller.update(ang_rob)                                # Update controller
-        # TODO: apply controll output to robot (
+        ang_ctrl_output = angle_controller.update(ang_rob)# Update controller
+        walker.turn(SAMPLE_TIME,ang_ctrl_output)
+
 
     # Posistion controller
     else:
         angle_controller.clear()                                        # Reset angle controller
         pos_ctrl_output = posistion_controller.update(PIDvar.get_dist_robot)              # Update posistion controller
+        walker.move(SAMPLE_TIME,pos_ctrl_output)
         # TODO: apply controller output to robot
 
     # Check waypoint (Without interupt)
--- src/control_system/pidvariables.py
+++ src/control_system/pidvariables.py
@@ -2,28 +2,29 @@ import math
 import brick_class
 import nxt_functions
 
+walker = brick_class.Walker()
+
 class PidVariables:
 
     def __init__(self):
 
         # 1D
-        self.dist_ref = 0
-        self.dist_robot = 0
-        self.ang_ref = 0
-        self.ang_robot = 0
+        self.dist_ref = 0.0
+        self.dist_robot = 0.0
+        self.ang_ref = 0.0
+        self.ang_robot = 0.0
 
 
         # 2D
-        self.xpos_robot = 0
-        self.ypos_robot = 0
-        self.xpos_ref = 0
-        self.ypos_ref = 0
-        self.walker = brick_class.Walker()
+        self.xpos_robot = 0.0
+        self.ypos_robot = 0.0
+        self.xpos_ref = 0.0
+        self.ypos_ref = 0.0
 
     def set_new_waypoint(self):                                                 # should alter the waypoint for the posiston
         # TODO: new xref and yref from matrix
-        self.xpos_ref = 0
-        self.ypos_ref = 0
+        self.xpos_ref = 0.0
+        self.ypos_ref = 0.0
         pass
 
     def get_ang_ref(self):
@@ -31,10 +32,12 @@ class PidVariables:
         adj = self.xpos_ref - self.xpos_robot                                   # Update varibles before use
         opp = self.ypos_ref - self.ypos_robot
         self.ang_ref = math.degrees(math.atan2(opp, adj))
-        return self.ang_ref
+        #return self.ang_ref
+        #Dette er en testkode
+        return 90.0
 
     def get_ang_robot(self):
-        self.ang_robot = 0
+        self.ang_robot = 0.0
         self.ang_robot = nxt_functions.get_compass()
         return self.ang_robot
 
@@ -53,10 +56,10 @@ class PidVariables:
 
     def update_coordinates(self):
         # TODO: get robot pos from matrix
-        self.xpos_robot = 0  # get value from image
-        self.ypos_robot = 0  # get value from image
-        self.xpos_ref = 0
-        self.ypos_ref = 0
+        self.xpos_robot = 0.0  # get value from image
+        self.ypos_robot = 0.0 # get value from image
+        self.xpos_ref = 0.0
+        self.ypos_ref = 0.0
 
 
 
